4e08c8c70451b4779eaa33b0d9a5265ecc9c0d78
annaritadituri <annaritadituri@icloud.com>
2024-06-17 22:38:26 +0200

    [UPD] Added basic style to show.php

--- show.php
+++ show.php
@@ -15,19 +15,27 @@ $length_g = strlen($good_paragraph);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords Show</title>
+
+    <!--style-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-    <p>
-        Il paragrafo inviato è lungo: <?php echo $length_p ?>
-    </p>
-    <p>
-        <?php echo $paragraph ?>
-    </p>
-    <p>
-        Il paragrafo inviato è lungo: <?php echo $length_g ?>
-    </p>
-    <p>
-        <?php echo $good_paragraph ?>
-    </p>
+<body class="bg-info-subtle">
+
+    <div class="container text-center mt-5">
+
+        <p>
+            Il paragrafo inviato è lungo: <?php echo $length_p ?>
+        </p>
+        <p>
+            <?php echo $paragraph ?>
+        </p>
+        <p>
+            Il paragrafo censurato è lungo: <?php echo $length_g ?>
+        </p>
+        <p>
+            <?php echo $good_paragraph ?>
+        </p>
+
+    </div>
 </body>
 </html>
