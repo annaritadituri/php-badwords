2eb676e82cf23ff3aed79135736b65cd7551dee2
annaritadituri <annaritadituri@icloud.com>
2024-06-17 22:34:41 +0200

    [UPD] Added basic style to index.php

--- index.php
+++ index.php
@@ -4,24 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
+
+    <!--style-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-info-subtle">
 
-    <form action="show.php" method="GET">
+    <div class="container text-center px-5">
 
-        <div>
-            <label for="paragraph">Paragraph</label>
-            <input type="text" name="paragraph" id="paragraph">
-        </div>
-        <div>
-            <label for="word">Word to hide</label>
-            <input type="text" name="word" id="word">
-        </div>
-        <div>
-            <button>Invia</button>
-        </div>
+        <h1 class="text-info my-5">Scrivi un paragrafo da censurare</h1>
 
-    </form>
+        <form action="show.php" method="GET">
+
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingTextarea2" name="paragraph">
+                <label for="floatingTextarea2">Paragraph</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingTextarea" name="word">
+                <label for="floatingTextarea">Word to hide</label>
+            </div>
+            <div>
+                <button type="submit" class="btn btn-info text-white p-3 px-4">Invia</button>
+            </div>
+
+        </form>
+
+    </div>
+
+    
     
 </body>
 </html>
